a919424798ad53ffe7a6a1b142641a30d3ca85c0
KacperOmen <komen1@stu.vistula.edu.pl>
2025-12-06 14:41:13 +0100

    Stworzenie modelu User + połączenie z bazą danych + stworzenie pliku .env do przechowywania sekretów oraz .env.example który pokazuje jak co zawrzeć w pliku .env

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
 const app = express()
 
-app.get('/test', (req, res) => {
-    res.json('ok')
-})
+dotenv.config()
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect("mongodb+srv://user:lXnIB0fAPtqYthj5@blogcluster.hm2ilky.mongodb.net/?appName=BlogCluster")
+.then(() => {
+    console.log("Connected to database")
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    })
+})
+.catch(() => {
+    console.log("Connection failed")
+})
